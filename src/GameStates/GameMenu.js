be1e919ac1e830ed00a4be66581ee731396c20dd
Zelenskiy George <pogapwnz@gmail.com>
2021-10-04 20:11:41 +0200

    HF small bug with strings passed as int prop

--- src/GameStates/GameMenu.js
+++ src/GameStates/GameMenu.js
@@ -6,21 +6,21 @@ function GameMenu({lives, mobs, speed, startGame, updateContext}) {
         <form>
             <label>
                 Speed:
-                <select value={speed} onChange={event => updateContext({speed: event.target.value})}>
+                <select value={speed} onChange={event => updateContext({speed: parseInt(event.target.value)})}>
                     {Array.from(Array(9)).map((element, index) =>
                         <option key={index + 1} value={index + 1}>{index + 1}</option>)}
                 </select>
             </label>
             <label>
                 Mobs count:
-                <select value={mobs} onChange={event => updateContext({mobs: event.target.value})}>
+                <select value={mobs} onChange={event => updateContext({mobs: parseInt(event.target.value)})}>
                     {Array.from(Array(4)).map((element, index) =>
                         <option key={index + 1} value={index + 1}>{index + 1}</option>)}
                 </select>
             </label>
             <label>
                 Lives:
-                <select value={lives} onChange={event => updateContext({lives: event.target.value})}>
+                <select value={lives} onChange={event => updateContext({lives: parseInt(event.target.value)})}>
                     {Array.from(Array(9)).map((element, index) =>
                         <option key={index + 1} value={index + 1}>{index + 1}</option>)}
                 </select>
