34192bac6c5473fb1cbc377bba65d581c0163228
Zelenskiy George <pogapwnz@gmail.com>
2022-02-10 00:01:42 +0100

    Use edges in the Mob intersection-with-the-dge computing

--- src/GameEngine/Mob.js
+++ src/GameEngine/Mob.js
@@ -12,6 +12,8 @@ export class Mob {
         this.y = y;
         this.velocity_x = velocity_x;
         this.velocity_y = velocity_y;
+        this.edges = null;
+        this.nextVertex = null;
     }
 
     static randomize(velocity, max_x, max_y) {
@@ -23,83 +25,79 @@ export class Mob {
         );
     }
 
-    calcNextStep(
-        vertexes = [
-            { x: 0, y: 0 },
-            { x: 800, y: 0 },
-            { x: 800, y: 600 },
-            { x: 0, y: 600 },
-        ],
-        player
-    ) {
-        // COLLISION CHECK
-        // 1.Player collision check first Because it causes endGame
-        //
-        // 2. I've taken a brief look at the SAT to detect collision, but it seems too complex for my case
+    // COLLISION CHECK
+    // 1.Player collision check first Because it causes endGame
+    //
+    // 2. I've taken a brief look at the SAT to detect collision, but it seems too complex for my case
 
-        // PLAYER COLLISION
-        // ...
+    // PLAYER COLLISION
+    // ...
 
-        // BOUNDARY COLLISION
-        // 1.list of conditions which can cause velocity inversion (currently in Mob layer only border collision is checked)
+    // BOUNDARY COLLISION
+    // 1.list of conditions which can cause velocity inversion (currently in Mob layer only border collision is checked)
 
-        // 3.
-        // vertexes - always even, >= 4
-        // edges - always even >=4
+    // 3.
+    // vertexes - always even, >= 4
+    // edges - always even >=4
 
-        // EDGES basic - basic coord diff
-        // const edges = vertexes.map((vertex, index, array) => {
-        //     const nextVertex =
-        //         index + 1 !== array.length ? array[index + 1] : array[0];
-        //     return { x: nextVertex.x - vertex.x, y: nextVertex.y - vertex.y };
-        // });
+    // The most obvious solution, then use EDGES v2
 
-        // EDGES v2 - reduce vertexes into two maps of horizontal/vertical edges. probably try with trees later
-        // const edges_h = {};
-        // const edges_v = {};
-        // vertexes.forEach((vertex, index, array) => {
-        //     const nextVertex =
-        //         index + 1 !== array.length ? array[index + 1] : array[0];
-        //     if (index % 2) {
-        //         // even - horizontal, vertex.y===nextVertex.y
-        //         if (edges_h[vertex.y]) {
-        //             edges_h[vertex.y].push(vertex.x, nextVertex.x);
-        //         } else {
-        //             edges_h[vertex.y] = [vertex.x, nextVertex.x];
-        //         }
-        //     } else {
-        //         // odd - vertical
-        //         if (edges_v[vertex.x]) {
-        //             edges_v[vertex.x].push(vertex.y, nextVertex.y);
-        //         } else {
-        //             edges_v[vertex.x] = [vertex.y, nextVertex.y];
-        //         }
-        //     }
-        // });
+    // EDGES basic - basic coord diff
+    // const edges = vertexes.map((vertex, index, array) => {
+    //     const nextVertex =
+    //         index + 1 !== array.length ? array[index + 1] : array[0];
+    //     return { x: nextVertex.x - vertex.x, y: nextVertex.y - vertex.y };
+    // });
 
-        // The most obvious solution, then use EDGES v2
-        vertexes.forEach((vertex, index, array) => {
-            const nextVertex =
-                index + 1 !== array.length ? array[index + 1] : array[0];
-            if (nextVertex.x === vertex.x) {
-                //vertical edge
-                const inverted = this.velocity_x < 0;
-                const x = inverted ? this.x : this.x + MOB_WIDTH;
+    // EDGES v2 - reduce vertexes into two maps of horizontal/vertical edges. probably try with trees later
+    // const edges_h = {};
+    // const edges_v = {};
+    // vertexes.forEach((vertex, index, array) => {
+    //     const nextVertex =
+    //         index + 1 !== array.length ? array[index + 1] : array[0];
+    //     if (index % 2) {
+    //         // even - horizontal, vertex.y===nextVertex.y
+    //         if (edges_h[vertex.y]) {
+    //             edges_h[vertex.y].push(vertex.x, nextVertex.x);
+    //         } else {
+    //             edges_h[vertex.y] = [vertex.x, nextVertex.x];
+    //         }
+    //     } else {
+    //         // odd - vertical
+    //         if (edges_v[vertex.x]) {
+    //             edges_v[vertex.x].push(vertex.y, nextVertex.y);
+    //         } else {
+    //             edges_v[vertex.x] = [vertex.y, nextVertex.y];
+    //         }
+    //     }
+    // });
+    move(edges, player) {
+        //Edges were updated, need to recompute the closest intersection.
+        if (edges !== this.edges) {
+            this.edges = edges;
+            this._computeIntersection(edges);
+        }
+    }
+
+    _computeIntersection(edges = this.edges) {
+        // Closest edge computing
+        edges.forEach((edge) => {
+            if (edge.hasOwnProperty('x')) {
+                // Vertical
                 if (
-                    inverted &&
-                    vertex.x < x &&
-                    vertex.x > this.velocity_x + x
-                ) {
-                    //IT CROSSES THE LINE, CHECK IF IT CROSSES EDGE
-                } else if (
-                    !inverted &&
-                    vertex.x < x &&
-                    vertex.x > this.velocity_x + x
-                ) {
-                    //IT CROSSES THE LINE, CHECK IF IT CROSSES EDGE (same as inverted)
-                }
+                    (this.velocity_x < 0 && this.x < edge.x) ||
+                    (this.velocity_x > 0 && this.x > edge.x)
+                )
+                    return;
+                // TODO find intersection
             } else {
-                //horizontal edge
+                // Horizontal
+                if (
+                    (this.velocity_y < 0 && this.y < edge.y) ||
+                    (this.velocity_y > 0 && this.y > edge.y)
+                )
+                    return;
+                // TODO find intersection
             }
         });
     }
