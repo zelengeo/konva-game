275db24cbf3d86fc5b02480cb33fee122bef527b
Zelenskiy George <pogapwnz@gmail.com>
2022-02-21 23:13:46 +0100

    Add Appearence context (thema, scaling, some other visuals) -also prettify

--- src/App.js
+++ src/App.js
@@ -1,13 +1,59 @@
-import React from 'react';
-import Game from './Game';
-import WithDebugContext from './Utils/withDebugContext';
+import React, { useContext } from 'react';
+import { useMachine } from '@xstate/react';
+import gameStateMachine, { STATES } from './Utils/gameStateMachine';
+import WithDebugContext, { DebugContext } from './Utils/withDebugContext';
+import GameMenu from './GameStates/GameMenu';
+import GameResults from './GameStates/GameResults';
+import Game from './GameStates/Game';
 
 function App() {
-    return (
-        <WithDebugContext>
-            <Game />
-        </WithDebugContext>
-    );
+    const [current, send] = useMachine(gameStateMachine);
+
+    const debug = useContext(DebugContext);
+    debug.state && console.log('GAME STATE', current);
+
+    let children;
+    switch (current.value) {
+        case STATES.MENU:
+            children = (
+                <GameMenu
+                    {...current.context}
+                    startGame={() => send('START')}
+                    updateContext={(nextContext) => {
+                        send({ type: 'UPDATE_CONTEXT', context: nextContext });
+                    }}
+                />
+            );
+            break;
+        case STATES.GAME:
+            children = (
+                <Game
+                    {...current.context}
+                    toResults={() => send('END_GAME_SESSION')}
+                    toMenu={() => send('RETURN_TO_MENU')}
+                />
+            );
+            break;
+        case STATES.RESULT:
+            children = (
+                <GameResults
+                    {...current.context}
+                    toMenu={() => send('END')}
+                    restartGame={() => send('RESTART')}
+                />
+            );
+            break;
+        default:
+            children = (
+                <GameMenu
+                    {...current.context}
+                    startGame={() => send('START')}
+                />
+            );
+    }
+
+    // switch with render-props components for menu and result. And game
+    return <WithDebugContext>{children}</WithDebugContext>;
 }
 
 export default App;
---
+++ src/Canvas/AppearanceContext.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const APPEARANCE_DEFAULT_VALUES = { scale: 3 };
+
+export const AppearanceContext = React.createContext(APPEARANCE_DEFAULT_VALUES);
+
+export default AppearanceContext;
