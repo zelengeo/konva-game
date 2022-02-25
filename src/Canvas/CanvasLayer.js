7ca2e5aaa81f57e247e41a6a6be06ff7ebafb1f1
Zelenskiy George <pogapwnz@gmail.com>
2022-02-25 23:53:07 +0100

    Prettify and leftovers

--- src/Canvas/CanvasLayer.js
+++ src/Canvas/CanvasLayer.js
@@ -1,42 +1,40 @@
-import React, {useRef, useEffect, useContext} from 'react';
+import React, { useRef, useEffect, useContext } from 'react';
 import PropTypes from 'prop-types';
-import {DebugContext} from "../Utils/withDebugContext";
-import {getCtx} from "../Utils/helpers";
+import { DebugContext } from '../Utils/withDebugContext';
+import { getCtx } from '../Utils/helpers';
 
-const CanvasLayer = ({posX, posY, width, height}) => {
+const CanvasLayer = ({ posX, posY, width, height }) => {
     const debug = useContext(DebugContext);
-    debug.render && console.log("Canvas layer rerender", {posX, posY, width, height})
+    debug.render &&
+        console.log('Canvas layer rerender', { posX, posY, width, height });
 
     const canvasRef = useRef(null);
-    useEffect(()=>{
-        debug.canvas && console.log("Canvas redraw at useEffect:", {posX, posY});
+    useEffect(() => {
+        debug.canvas &&
+            console.log('Canvas redraw at useEffect:', { posX, posY });
         const ctx = getCtx(canvasRef);
         ctx.clearRect(0, 0, width, height);
         ctx.fillStyle = 'rgb(0, 0, 200)';
         ctx.fillRect(posX, posY, 20, 20);
         //FIXME: debug value causes deps warning
         // eslint-disable-next-line react-hooks/exhaustive-deps
-    },[posX, posY, width, height])
+    }, [posX, posY, width, height]);
 
-    return <canvas width={width}
-                   height={height}
-                   ref={canvasRef}
-    />
-
+    return <canvas width={width} height={height} ref={canvasRef} />;
 };
 
 CanvasLayer.defaultProps = {
     width: 800,
     height: 600,
     posX: 0,
-    posY: 0
+    posY: 0,
 };
 
 CanvasLayer.propTypes = {
     width: PropTypes.number,
     height: PropTypes.number,
     posX: PropTypes.number,
-    posY: PropTypes.number
+    posY: PropTypes.number,
 };
 
 export default CanvasLayer;
--- src/Canvas/CanvasStage.js
+++ src/Canvas/CanvasStage.js
@@ -1,21 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import AvatarLayer from './AvatarLayer';
+// import PlayerLayer from './PlayerLayer';
 import GridLayer from './GridLayer';
 import MobLayer from './MobLayer';
 
-function CanvasStage({ gameBoard }) {
+function CanvasStage({ gameCore }) {
+    //TODO
+    // Prepare nice error component
+    if (!gameCore) return null;
+
     return (
-        <div className={'canvas-stage-container'}>
-            <GridLayer gameBoard={gameBoard} />
-            <MobLayer gameBoard={gameBoard} />
-            <AvatarLayer gameBoard={gameBoard} />
+        <div>
+            <GridLayer gameCore={gameCore} />
+            <MobLayer gameCore={gameCore} />
+            {/*<PlayerLayer gameCore={gameCore} />*/}
         </div>
     );
 }
 
 CanvasStage.propTypes = {
-    gameBoard: PropTypes.object,
+    gameCore: PropTypes.object,
 };
 
 export default CanvasStage;
